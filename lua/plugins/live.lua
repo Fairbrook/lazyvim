39b4eb50312aced16a1d32bf4238679639224dab
Ubuntu <ubuntu@covenant-workstation.lxd>
2025-07-11 14:31:10 +0000

    lazy lua config

---
+++ lua/plugins/live.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "azratul/live-share.nvim",
+    dependencies = {
+      "jbyuki/instant.nvim",
+    },
+    config = function()
+      require("live-share.provider").register("live-share-socat", {
+        command = function(cfg, port, service_url)
+          return string.format("live-share-socat %d %d > %s", port, port, service_url)
+        end,
+        pattern = "%d+.%d+.%d+.%d+ %d+",
+      })
+      vim.g.instant_username = "kevinm"
+      require("live-share").setup({
+        service = "live-share-socat",
+      })
+    end,
+  },
+}
